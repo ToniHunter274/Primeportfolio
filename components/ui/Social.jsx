3017365025a62a5a8dbf15eabd2a1564d82277c6
Toniprime <Toniprime15@gmail.com>
2024-09-24 13:20:40 +0100

    added

---
+++ components/ui/Social.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const socials = [
+  { icon: <FaGithub />, path: "https://github.com"},
+  { icon: <FaLinkedinIn />, path: "https://linkedin.com"},
+  { icon: <FaYoutube />, path: "https://youtube.com"},
+  { icon: <FaTwitter />, path: "https://twitter.com"},
+  
+];
+
+const Social = ({containerStyles, iconStyles}) => {
+  return (
+    <div className={containerStyles}>
+        {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Social;
